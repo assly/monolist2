94aec33efa1f8d8e3b8400d0a6dfa3743bbe15ca
Aoshi_F <blue_20will@yahoo.co.jp>
2016-05-27 09:37:26 +0000

    modified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
-    
-    def item_image_tag(item)
-        image_tag(item.large_image , alt: item.title)
-    end
+  def item_image_tag(item)
+    image_tag(item.large_image , alt: item.title)
+  end
 end
